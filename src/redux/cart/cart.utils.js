4fb0b56170771dd663e45f26f9152c86e6021f06
Reginaldo <juniorrga@gmail.com>
2021-06-01 08:24:06 -0300

    added reselect

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,11 +2,15 @@ export const addItemToCart = (items, itemToAdd) => {
   const existingItem = items.find(item => item.id === itemToAdd.id);
 
   if (existingItem) {
-    return items.map(item => item.id === itemToAdd.id
+    const newItems = items.map(item => item.id === itemToAdd.id
       ? { ...item, quantity: item.quantity + 1 }
-      : { item }
+      : item
     )
+
+    return newItems;
   }
 
-  return [...items, { ...itemToAdd, quantity: 1 }]
+  const newItems =  [...items, { ...itemToAdd, quantity: 1 }]
+
+  return newItems;
 }
